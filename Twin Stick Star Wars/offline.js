077793a0718514561887aae80247b827d87f6f96
gamer4maker <gamer4maker@users.noreply.github.com>
2017-02-24 04:07:44 +1100

    Add files via upload

---
+++ Twin Stick Star Wars/offline.js
@@ -0,0 +1,29 @@
+﻿{
+	"version": 1487869021,
+	"fileList": [
+		"data.js",
+		"c2runtime.js",
+		"jquery-2.1.1.min.js",
+		"images/player-sheet0.png",
+		"images/tiledbackground.png",
+		"images/bullet-sheet0.png",
+		"images/enemy-sheet0.png",
+		"images/explosion-sheet0.png",
+		"images/explosion-sheet1.png",
+		"images/radar-sheet0.png",
+		"images/radar-sheet1.png",
+		"images/radar-sheet2.png",
+		"images/blip-sheet0.png",
+		"images/radarframe-sheet0.png",
+		"images/tiledbackground2.png",
+		"media/depthchargeshort.m4a",
+		"media/depthchargeshort.ogg",
+		"media/laserblast.ogg",
+		"icon-16.png",
+		"icon-32.png",
+		"icon-114.png",
+		"icon-128.png",
+		"icon-256.png",
+		"loading-logo.png"
+	]
+}
